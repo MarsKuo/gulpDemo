ef2b66d593ebd3328d2a461f6ad7dc0c7b6c6669
Mars <talent.seek@msa.hinet.net>
2017-11-22 17:33:54 +0800

    Update 2017-11-22T09:33:43.248Z

---
+++ javascripts/all.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var newfunction = function newfunction() {
+    console.log('hello');
+};
+newfunction();
+var newfunction2 = function newfunction2() {
+    console.log('b');
+};
+
+newfunction2();
+//# sourceMappingURL=all.js.map
